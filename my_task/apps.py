0ca30161991d99dde39fbd82b965df287373d49d
Nordenchik <nordenchik228@gmail.com>
2026-08-08 13:29:21 +0300

    v1.0.1

--- my_task/apps.py
+++ my_task/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
 class MyTaskConfig(AppConfig):
     name = 'my_task'
--- my_task/models.py
+++ my_task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000, help_text='Опишіть завдання...')
+    description = models.CharField(max_length=1000, help_text='Опишіть завдання.')
     status = models.CharField(max_length=100)
     priority = models.IntegerField()
     progress_termin = models.DateField()
