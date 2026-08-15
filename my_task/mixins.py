655a7117ef0da7f8c549bce7421518f3051983c2
Nordenchik <nordenchik228@gmail.com>
2026-08-15 14:18:41 +0300

    v2.6.1

--- my_task/mixins.py
+++ my_task/mixins.py
@@ -4,6 +4,5 @@ class UserIsOwnerMixin:
     owner_field = "author"
     def dispatch(self, request, *args, **kwargs):
         obj = self.get_object()
-        if getattr(obj, self.owner_field, None) != request.user:
-            raise PermissionDenied
+        if getattr(obj, self.owner_field, None) != request.user: raise PermissionDenied
         return super().dispatch(request, *args, **kwargs)
