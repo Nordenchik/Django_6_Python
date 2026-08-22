52f62cd068b612abdf0d3914c97b96224e0d96af
Nordenchik <nordenchik228@gmail.com>
2026-08-22 14:03:31 +0300

    v3.0.0

--- my_task/models.py
+++ my_task/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Task(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=1000, help_text='Опишіть завдання.')
+    description = models.CharField(max_length=1000, help_text='Опишіть завдання.', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', help_text='Вставте зображення для завдання.', null=True, blank=True)
     status = models.CharField(max_length=100)
     priority = models.IntegerField()
-    progress_termin = models.DateField()
+    progress_termin = models.DateField(null=True, blank=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self): return self.name
--- my_task/views.py
+++ my_task/views.py
@@ -5,39 +5,42 @@ from .models import Task
 from .mixins import UserIsOwnerMixin
 
 class TaskList(ListView):
-    model = Task()
+    model = Task
     template_name = 'template.html'
     context_object_name = 'tasks'
 
 class TaskDetail(DetailView):
-    model = Task()
+    model = Task
     template_name = 'task_detail.html'
     context_object_name = 'task'
 
 class TaskCreate(LoginRequiredMixin, CreateView):
-    model = Task()
+    model = Task
     fields = ['name', 'description', 'status', 'priority', 'progress_termin']
     template_name = 'task_create.html'
     context_object_name = 'task'
+    success_url = '/'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
-    model = Task()
+    model = Task
     fields = ['name', 'description', 'status', 'priority', 'progress_termin']
     template_name = 'task_update.html'
     context_object_name = 'task'
+    success_url = '/'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 class TaskDelete(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
-    model = Task()
+    model = Task
     template_name = 'task_update.html'
     context_object_name = 'task'
+    success_url = '/'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
